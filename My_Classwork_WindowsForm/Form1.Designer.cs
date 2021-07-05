f29ba00e7ad82307db46968b3823233de18da7f7
kishman7 <kishman_us@ukr.net>
2021-07-06 00:03:39 +0300

    Add Homework_WindowsForm

---
+++ My_Classwork_WindowsForm/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace My_Classwork_WindowsForm
+{
+    partial class FormUkraine
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormUkraine));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OpacityPlus = new System.Windows.Forms.Button();
+            this.OpacityMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(66, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(348, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(128, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // OpacityPlus
+            // 
+            this.OpacityPlus.BackColor = System.Drawing.Color.Yellow;
+            this.OpacityPlus.Location = new System.Drawing.Point(200, 25);
+            this.OpacityPlus.Name = "OpacityPlus";
+            this.OpacityPlus.Size = new System.Drawing.Size(75, 23);
+            this.OpacityPlus.TabIndex = 3;
+            this.OpacityPlus.Text = "Opacity +";
+            this.OpacityPlus.UseVisualStyleBackColor = false;
+            this.OpacityPlus.Click += new System.EventHandler(this.OpacityPlus_Click);
+            // 
+            // OpacityMinus
+            // 
+            this.OpacityMinus.BackColor = System.Drawing.Color.Yellow;
+            this.OpacityMinus.Location = new System.Drawing.Point(200, 97);
+            this.OpacityMinus.Name = "OpacityMinus";
+            this.OpacityMinus.Size = new System.Drawing.Size(75, 23);
+            this.OpacityMinus.TabIndex = 4;
+            this.OpacityMinus.Text = "Opacity -";
+            this.OpacityMinus.UseVisualStyleBackColor = false;
+            this.OpacityMinus.Click += new System.EventHandler(this.OpacityMinus_Click);
+            // 
+            // FormUkraine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(511, 384);
+            this.Controls.Add(this.OpacityMinus);
+            this.Controls.Add(this.OpacityPlus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormUkraine";
+            this.Text = "FormUkraine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OpacityPlus;
+        private System.Windows.Forms.Button OpacityMinus;
+    }
+}
+
---
+++ My_Classwork_WindowsForm/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Classwork_WindowsForm
+{
+    public partial class FormUkraine : Form
+    {
+        public FormUkraine()
+        {
+            InitializeComponent();
+        }
+
+        //Створити дві кнопки. Лічити натиснення кожної з кнопок. Результати(2 лічильники) виводити на деякий Label.
+        static int count1 = 0;
+        static int count2 = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count1++;
+            label1.Text = ($"Counter for button1 = {count1}, Counter for button2 = {count2}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count2++;
+            label1.Text = ($"Counter for button1 = {count1}, Counter for button2 = {count2}");
+        }
+
+        //Створити додаток з елементом Label.При натисненнні на Label, він повинен переміщатися випадково, але не повинен виходити за границі форми.
+        Random rnd = new Random();
+        private void label1_Click(object sender, EventArgs e)
+        {
+            ClientSize = new Size(500, 400);
+            for (int i = 0; i < 20; i++)
+            {
+                label1.Top = rnd.Next(1, 400);
+                label1.Left = rnd.Next(1, 400);
+                Thread.Sleep(500);
+            }
+            
+            //Top = 0;
+            //Left = 0;
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    int X = rnd.Next(50, 400);
+            //    int Y = rnd.Next(50, 300);
+            //    label1.Location = new Point(X, Y);
+            //    Thread.Sleep(1000);
+            //}
+            
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    label1.Location = templocation;
+            //    templocation.X = rnd.Next(100, 527);
+            //    label1.Location = templocation;
+            //    templocation.Y = rnd.Next(100, 423);
+            //    Thread.Sleep(1000);
+            //}
+                
+           
+        }
+
+        //При натисненні деякої кнопки збільшувати прозорість форми, 
+        //іншої кнопки - зменшувати прозорість форми(властивість Opacity : 0..1).
+        private void OpacityPlus_Click(object sender, EventArgs e)
+        {
+            Opacity -= 0.1;
+        }
+
+        private void OpacityMinus_Click(object sender, EventArgs e)
+        {
+            Opacity += 0.1;
+        }
+    }
+}
